bdd9878ecb1baeb8c66cab1b05e8be4fb99553f8
Puzhao (Kevin) Zhang <puzh@dhigroup.com>
2024-01-30 09:12:27 +0100

    u_test sign

--- dhi_sdg661_t_test.py
+++ dhi_sdg661_t_test.py
@@ -4,9 +4,15 @@ from pathlib import Path
 import matplotlib.pyplot as plt
 import dask
 import dask.dataframe as dd
+import scipy.stats as stats
 
-dtypes = {"aggregation_year": 'uint16',"permanent_area": 'float64',"seasonal_area": 'float64',
-          "maybepermanent": 'float64',"maybeseasonal": 'float64'
+import warnings
+warnings.filterwarnings("ignore")
+
+dtypes = {"aggregation_year": 'uint16', "permanent_area": 'float64',
+          "seasonal_area": 'float64',
+          "maybepermanent": 'float64',
+          "maybeseasonal": 'float64'
          }
 
 data_dir = Path.home() / 'pz'
@@ -16,12 +22,14 @@ cols_required = ['permanent_area',
                 ]
 arr_mean_std = []
 
-meta = {'id_bgl': 'object', 'start_year': int, 'basin_level': int} #'id_bgl': 'object', 'start_year': int, 'basin_level': int
-meta.update({col: 'float16' for col in cols_required})
+meta = {'id': 'str', 't_score': 'float', 'u_score': 'float', 'p_t': 'float', 'p_u': 'float', 'reject_null': 'bool'} #'id_bgl': 'object', 'start_year': int, 'basin_level': int
+meta_adm0 = {'id': 'str', 'adm0_name': 'str', 't_score': 'float', 'u_score': 'float', 'p_t': 'float', 'p_u': 'float', 'reject_null': 'bool'} #'id_bgl': 'object', 'start_year': int, 'basin_level': int
+# meta.update({col: 'float16' for col in cols_required})
 
-def t_test_and_u_test(group, basin_level):
+def t_test_and_u_test(group, p_thd = 0.001):
     id = group.index[0] # basin_id
-    patch = basin[(basin[id_col_name] == id) & (basin['aggregation_year']  >=2000)]
+    
+    patch = group[(group.index == id) & (group['aggregation_year']  >=2000)]
     data1 = list(patch[patch['aggregation_year'] < 2020]['permanent_area'].values)
     data2 = list(patch[patch['aggregation_year'] >= 2017]['permanent_area'].values)
 
@@ -36,97 +44,44 @@ def t_test_and_u_test(group, basin_level):
     # P-values close to 0 indicate that the observed difference is unlikely to be due to chance, (true difference?)
     # whereas a P value close to 1 suggests no difference between the groups other than due to chance (no difference?)
     reject_null = (p_t < p_thd) or (p_u < p_thd) # True: different, False: no difference
-    df = pd.DataFrame([id, t_score, u_score, p_t, p_u, reject_null], columns=['id', 't_score', 'u_score', 'p_t', 'p_u', 'reject_null'])
+
+    df = pd.DataFrame([[id, t_score, u_score, p_t, p_u, float(reject_null)]], columns=['id', 't_score', 'u_score', 'p_t', 'p_u', 'reject_null'])
     return df
 
 
-def calculate_delta(group, basin_level, epision=1e-15):
-    # group = group.reset_index() # needed for drop
-    id = group.index[0] # basin_id
-    
-    df_2000_2019 = group[ (2000 <= group['aggregation_year']) & (group['aggregation_year']<= 2019)]
-    beta_baseline_median = df_2000_2019[cols_required].median()
 
-    arr_delta = []
-    for start_year in [2000, 2005, 2010, 2015, 2017]:
-    # for start_year in [2017]:
-        df_5y = group[ (start_year <= group['aggregation_year']) & (group['aggregation_year'] < start_year + 5)]
-        gamma_5y_median = df_5y[cols_required].median()
-        
-        delta = (gamma_5y_median - beta_baseline_median) / (beta_baseline_median + epision) * 100
-
-        delta_cols = list([delta[col].values for col in [cols_required]][0])
-        arr_delta.append([id, start_year, basin_level] + delta_cols)
-
-    df_delta = pd.DataFrame(data=arr_delta, columns=['id_bgl', 'start_year', 'basin_level'] + cols_required)
-    return df_delta
-
-def remove_outliers(df, p_low, p_high):
-    for col in ['permanent_area']:
-        select = (df[col] > p_low[col]) & (df[col] < p_high[col])
-        # select = (df[col] < p_high[col])
-        df = df[select]
-        # df = df.drop(index=df[df[col] <= p1[col]].index)
-        # df = df.drop(index=df[df[col] >= p99[col]].index)
-    return df
 
 if __name__ == '__main__':
     from dask.distributed import Client, LocalCluster
     cluster = LocalCluster(dashboard_address=':38787')
     client = Client(cluster)#timeout
 
-    folder = "Reservoirs" # Reservoirs, Pemanent_water
-    epision = 1e-15
-    
-    if "Pemanent_water" == folder:
-        p_low = 2 # remove lowest 1%
-        p_high = 98 # remove highest 4%
+    for folder in ["Pemanent_water"]: # Reservoirs, Pemanent_water
 
-    if "Reservoirs" == folder:
-        p_low = 2 # remove lowest 1%
-        p_high = 98 # remove highest 4%
-    
-    
-    output_dir = data_dir / f"outputs_V2_p{p_low}_p{p_high}_e15" / folder
-    output_dir.mkdir(exist_ok=True, parents=True)
-    print(output_dir)
+        output_dir = data_dir / f"outputs_ttest" / folder
+        output_dir.mkdir(exist_ok=True, parents=True)
+        print(output_dir)
 
-    for basin_level in range(3, 9):
-        print()
-        print(f"basins_level: {basin_level}")
-    
-        url = data_dir / folder / f"basins_level_{basin_level}_ts.csv"
-        basin = dd.read_csv(url, include_path_column=False, dtype=dtypes).repartition(npartitions=80).set_index(f'id_bgl_{basin_level}')
+        # # country-level analysis
+        # basin = dd.read_csv(data_dir / folder / 'gaul_0_ts.csv', include_path_column=False, dtype=dtypes).repartition(npartitions=80).set_index('adm0_code')
+        # df_delta = basin.groupby('adm0_code', group_keys=False).apply(t_test_and_u_test, meta=meta_adm0)
+        # df_delta.compute().to_csv(output_dir / f"gaul_0_t_test.csv")
+
+        # basin-level analysis
+        for basin_level in [0, 3, 4, 5, 6, 7, 8]:
+            print()
+            print(f"basins_level: {basin_level}")
         
-
-        # df_delta = basin.groupby(f'id_bgl_{basin_level}', group_keys=False).apply(calculate_delta, basin_level, epision, meta=meta)
-        df_delta = basin.groupby(f'id_bgl_{basin_level}', group_keys=False).apply(t_test_and_u_test, basin_level, meta=meta)
-        df_delta = df_delta.compute()
-
-        p_low_value = df_delta[cols_required].quantile(p_low * 0.01)#.compute()
-        p_high_value = df_delta[cols_required].quantile(p_high * 0.01)#.compute()
-
-        print(f"--------------------------------- basin_level: {basin_level}-----------------------------------")
-        print('p_low')
-        print(p_low_value)
-        print('p_high')
-        print(p_high_value)
-
-        df_delta.to_csv(output_dir / f"basins_level_{basin_level}_ts_delta.csv")
+            url = data_dir / folder / f"basins_level_{basin_level}_ts.csv"
+            basin = dd.read_csv(url, include_path_column=False, dtype=dtypes).repartition(npartitions=80).set_index(f'id_bgl_{basin_level}')
+            
+            ''' for debug '''
+            # number = 31
+            # df_delta = basin.groupby(f'id_bgl_{basin_level}', group_keys=False)
+            # t_test_and_u_test(df_delta.get_group("112_262").compute(), basin_level)
         
-        df = remove_outliers(df_delta, p_low_value, p_high_value)
-        print(df.describe())
+            # df_delta = basin.groupby(f'id_bgl_{basin_level}', group_keys=False).apply(calculate_delta, basin_level, epision, meta=meta)
+            df_delta = basin.groupby(f'id_bgl_{basin_level}', group_keys=False).apply(t_test_and_u_test, meta=meta).set_index('id')
         
-        # df_delta.visualize('test.png')
-        df.to_csv(output_dir / f"basins_level_{basin_level}_ts_delta_outliers_removed.csv")
-
-        # save hist
-        import matplotlib.pyplot as plt
-        for col in cols_required:
-            plt.figure()
-            df[col].plot(kind='hist', logy=True, bins=200)
-            plt.title(f'basin level: {basin_level}, {col}')
-            plt.savefig(output_dir / f"basins_level_{basin_level}_hist_{col}.png")
-            plt.close()
-
-        
+            df_delta = df_delta.compute()
+            df_delta.to_csv(output_dir / f"basins_level_{basin_level}_t_test.csv")
